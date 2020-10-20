eab8115ade9bb09b96353fd5403705f95e745507
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2020-10-20 15:41:31 +0900

    [add]usersテーブルにroll_flagを追加

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
+            $table->enum('roll_flag', ['st', 'te', 'ad']);
             $table->timestamps();
         });
     }
