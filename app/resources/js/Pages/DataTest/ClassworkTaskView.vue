f6e53f9c265d5e73ade605b745047930365a00dc
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2020-12-09 11:53:23 +0900

    [add]コードにコメントを追記

--- app/resources/js/Pages/DataTest/ClassworkTaskView.vue
+++ app/resources/js/Pages/DataTest/ClassworkTaskView.vue
@@ -12,12 +12,14 @@
                                 授業担当者： {{$page.in_charge.teacher.name}}<span v-for="tutor in $page.in_charge.tutors" :key="tutor.id">, {{tutor.name}}</span>
                             </div>
                         </div>
+                        <!-- 編集権限のあるユーザーの場合はボタンを表示 -->
                         <div v-if="$page.user.roll_flag == 'te' || $page.user.roll_flag == 'ad'">
                             <jet-button @click.native="showCreateForm()" class="text-lg">
                                 コンテンツ<br>追加
                             </jet-button>
                         </div>
                     </div>
+                    <!-- 新規作成フォーム -->
                     <div v-if="show_create_form" class="p-6 sm:px-20 bg-white border-b border-gray-200">
                         <create-task-form :in_charge_id="$page.in_charge.id" />
                     </div>
@@ -28,48 +30,50 @@
                         </div>
                     </div>
                     <!-- コンテンツが１つ以上ある場合 -->
-                    <div v-else v-for="classwork_task in $page.in_charge.classwork_tasks" :key="classwork_task.sort_num">
-                        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                            <div v-if="$page.user.roll_flag == 'te' || $page.user.roll_flag == 'ad'" class="flex space-x-4">
-                                <jet-button @click.native="showUpdateForm(classwork_task.id)">
-                                    ⚙
-                                </jet-button>
-                                <jet-danger-button @click.native="confirmClassworkTaskDeletion(classwork_task.id)">
-                                    削除
-                                </jet-danger-button>
-                            </div>
-                            <div v-if="show_update_form == classwork_task.id">
-                                <update-task-form :task="classwork_task" />
-                            </div>
-                            <div v-else>
-                                <h3 class="bg-white font-semibold text-xl text-gray-800 leading-tight">
-                                    content.name:{{classwork_task.name}}
-                                </h3>
-                                <div class="mt-6 text-gray-500">
-                                    <div v-for="content in classwork_task.contents" :key="content.sort_num">
-                                        <div v-if="content.type == 'text'">
-                                            {{content.text}}
-                                        </div>
-                                        <div v-else-if="content.type == 'link'">
-                                            <a :href="content.url" class="underline text-blue-600 visited:text-purple-600">
+                    <div v-else>
+                        <div v-for="classwork_task in $page.in_charge.classwork_tasks" :key="classwork_task.sort_num">
+                            <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                                <!-- 編集権限のあるユーザーの場合はボタンを表示 -->
+                                <div v-if="$page.user.roll_flag == 'te' || $page.user.roll_flag == 'ad'" class="flex space-x-4">
+                                    <jet-button @click.native="showUpdateForm(classwork_task.id)">
+                                        ⚙
+                                    </jet-button>
+                                    <jet-danger-button @click.native="confirmClassworkTaskDeletion(classwork_task.id)">
+                                        削除
+                                    </jet-danger-button>
+                                </div>
+                                <!-- 編集ボタンを押すとその部分の表示が編集フォームに置き換わる -->
+                                <!-- 編集フォーム -->
+                                <div v-if="show_update_form == classwork_task.id">
+                                    <update-task-form :task="classwork_task" />
+                                </div>
+                                <!-- コンテンツ情報の表示 -->
+                                <div v-else>
+                                    <h3 class="bg-white font-semibold text-xl text-gray-800 leading-tight">
+                                        content.name:{{classwork_task.name}}
+                                    </h3>
+                                    <div class="mt-6 text-gray-500">
+                                        <div v-for="content in classwork_task.contents" :key="content.sort_num">
+                                            <!-- typeに合わせて表示する内容を変更 -->
+                                            <div v-if="content.type == 'text'">
                                                 {{content.text}}
-                                            </a>
-                                        </div>
-                                        <div v-else-if="content.type == 'homework'">
-                                            <!-- 課題として登録されているファイル名の表示 -->
-                                            <div>ファイル名：{{content.file_name | replaceComma}}</div>
-                                            <!-- 課題提出ページへのリンク -->
-                                            <inertia-link :href="'/homework/' + classwork_task.id" class="underline text-blue-600 visited:text-purple-600">提出ページへ</inertia-link>
+                                            </div>
+                                            <div v-else-if="content.type == 'link'">
+                                                <a :href="content.url" class="underline text-blue-600 visited:text-purple-600">
+                                                    {{content.text}}
+                                                </a>
+                                            </div>
+                                            <div v-else-if="content.type == 'homework'">
+                                                <!-- 登録されているファイル名の表示 -->
+                                                <div>ファイル名：{{content.file_name | replaceComma}}</div>
+                                                <!-- 課題提出ページへのリンク -->
+                                                <inertia-link :href="'/homework/' + classwork_task.id" class="underline text-blue-600 visited:text-purple-600">提出ページへ</inertia-link>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="mt-6 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <div class="p-6 sm:px-20 bg-white border-b border-gray-800">
-                        $page.incharge: {{$page.in_charge}}
                     </div>
                 </div>
             </div>
@@ -122,11 +126,10 @@
 
         data() {
             return {
-                show_create_form: false,
-                show_update_form: 0,
+                show_create_form: false,    // 新規作成フォームの表示/非表示
+                show_update_form: 0,        // 編集フォームが表示されているコンテンツのid（classwork_task.idを格納）
 
-                confirmingClassworkTaskDeletion: false,
-                deleting: false,
+                confirmingClassworkTaskDeletion: false, // 削除モーダルの表示/非表示
 
                 form: this.$inertia.form({
                     '_method': 'DELETE',
@@ -138,24 +141,28 @@
         },
 
         methods: {
+            // 編集フォームの表示切替
             showUpdateForm(index) {
                 this.show_update_form = this.show_update_form != index ? index : 0;
             }, 
 
+            // 新規作成フォームの表示切替
             showCreateForm() {
                 this.show_create_form = !this.show_create_form;
             }, 
 
+            // 削除モーダルの表示切替
             confirmClassworkTaskDeletion(index) {
                 this.form.classwork_task_id = index;
-
                 this.confirmingClassworkTaskDeletion = true;
             },
 
+            // コンテンツ削除
             deleteClassworkTask() {
                 this.form.delete('/api/classwork-task/' + this.form.classwork_task_id, {
                     preserveScroll: true
                 }).then(response => {
+                    // 正常に削除できていれば削除モーダルを閉じる
                     if (! this.form.hasErrors()) {
                         this.confirmingUserDeletion = false;
                     }
