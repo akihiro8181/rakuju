db2683deb252eecf2548854127e819d04d00d1e5
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2020-12-24 14:50:31 +0900

    [add]カラーパレットとフォントの追加

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -13,6 +13,19 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            
+            colors: {
+                'dawn-orange':  '#eb6138ff',
+                'sunrise-glow': '#f18e1dff',
+                'blue-shadow':  '#404b73ff',
+                'warm-white':   '#f9ead2ff',
+                'ridge-gray':   '#7d93baff',
+                
+                'heavenly-blue':'#3362A8',
+                'early-morning':'#8489b6',
+                'cloud-sea':    '#ccdfef',
+                'clear-blue':   '#a1c3e7',
+            },
         },
     },
 
