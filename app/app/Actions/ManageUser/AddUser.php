2351cb40a9361ad9cd7927192a2bbde5e53bd7da
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2021-01-26 18:33:40 +0900

    [fix]同じ学校・同じlogin_numberのユーザーが重複しないように修正

--- app/app/Actions/ManageUser/AddUser.php
+++ app/app/Actions/ManageUser/AddUser.php
@@ -3,11 +3,13 @@
 namespace App\Actions;
 
 use App\Models\User;
+use App\Models\School;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Fortify\Rules\Password; // passwordのバリデーションの暫定対処用
 
 class AddUser
 {
@@ -23,14 +25,18 @@ class AddUser
      */
     public function add(User $current_user, array $input)
     {
+        Validator::make($input, [
+            'name' => ['required', 'string', 'max:255'],
+            'login_number' => ['required', 'regex:/^\s*-?[0-9,]+\s*$/', 'max:255'],
+            // passwordRules()の'confirmed'の条件を満たすには$input['password_confirmation']が必要
+            // 追加は後回しにして暫定対処
+            // 'password' => $this->passwordRules(),
+            'password' => ['required', 'string', new Password],
+        ])->validate();
 
-        // dd($input);
-
-        // Validator::make($input, [
-        //     'name' => ['required', 'string', 'max:255'],
-        //     'login_number' => ['required', 'string', 'max:255'],
-        //     'password' => $this->passwordRules(),
-        // ])->validate();
+        if (User::where('school_id', $current_user->school_id)->where('login_number', $input['login_number'])->exists()) {
+            return;
+        }
 
         User::create([
             'name' => $input['name'],
