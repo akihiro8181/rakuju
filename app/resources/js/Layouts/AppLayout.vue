0f87273ef137f547203f09de91e262331a1eeaf1
Yui-Tanaka-0804 <yui.tanaka.0804@gmail.com>
2021-01-27 14:12:50 +0900

    [謎のハンバーガー削除]

--- app/resources/js/Layouts/AppLayout.vue
+++ app/resources/js/Layouts/AppLayout.vue
@@ -120,16 +120,6 @@
 
                     </div>
 
-                    <!-- Hamburger -->
-                    <div class="-mr-2 flex items-center sm:hidden">
-                        <button @click="showingNavigationDropdown = ! showingNavigationDropdown" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                            <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                <path :class="{'hidden': showingNavigationDropdown, 'inline-flex': ! showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                                <path :class="{'hidden': ! showingNavigationDropdown, 'inline-flex': showingNavigationDropdown }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
-
                     <!-- new video window -->
                     <div>
                         <!-- 学生用のvideo リンク -->
